e4ebdf28246b355c17a6df9ecb90a73985f70e79
lucianoss <luciano22desouza@gmail.com>
2024-06-03 19:39:21 -0300

    Add files via upload
    Desafio Banco Digital

---
+++ Cliente.java
@@ -0,0 +1,13 @@
+
+public class Cliente {
+    private String nome;
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
